c0075af784a82c73a6beb4126c9b1ca398d61f2c
Eduardo Kramer <dududeoliveirakramer23@gmail.com>
2024-11-03 21:42:41 -0300

    Ajuste das configuracoes ao resize e classe ativa adicionada

--- js/slide.js
+++ js/slide.js
@@ -1,3 +1,5 @@
+import debounce from './debounce.js';
+
 export default class Slide{
   constructor(slide, wrapper){
     this.slide = document.querySelector(slide);
@@ -7,6 +9,7 @@ export default class Slide{
       startX: 0,
       movement: 0
     }
+    this.activeClass = 'active';
   } 
 
   transition(active){
@@ -78,12 +81,6 @@ export default class Slide{
     this.wrapper.addEventListener('touchend', this.onEnd);
   }
 
-  bindEvents(){
-    this.onStart = this.onStart.bind(this);
-    this.onMove = this.onMove.bind(this);
-    this.onEnd = this.onEnd.bind(this);
-  }
-
   //slides config
 
   slidePosition(slide){
@@ -112,12 +109,30 @@ export default class Slide{
     this.moveSlide(activeSlide.position);
     this.slidesIndexNav(index);
     this.dist.finalPosition = activeSlide.position
+    this.changeActiveClass();
   }
+  
+  changeActiveClass(){
+    this.slideArray.forEach(item => item.element.classList.remove(this.activeClass));
+    this.slideArray[this.index.active].element.classList.add(this.activeClass = 'active');
+  }
+
 
   activePrevSlide(){
     if (this.index.prev !== undefined){
       this.changeSlide(this.index.prev);
     }
+  }
+
+  onResize(){
+    setTimeout(() =>{
+      this.slidesConfig();
+      this.changeSlide(this.index.active)
+    }, 1000)
+  }
+
+  addResizeEvent(){
+    window.addEventListener('resize', this.onResize)
   }
 
   activeNextSlide(){
@@ -126,12 +141,20 @@ export default class Slide{
     }
   }
 
+  bindEvents(){
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+
+    this.onResize = debounce(this.onResize.bind(this), 200);
+  }
 
   init(){
     this.bindEvents();
     this.transition(true);
     this.addSlideEvents();
     this.slidesConfig();
+    this.addResizeEvent();
     return this;
   }
 }
